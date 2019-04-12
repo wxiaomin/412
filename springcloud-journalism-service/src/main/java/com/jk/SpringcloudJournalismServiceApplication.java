a32e900a10ee8a71eed629f8f16d10915287dab7
wxiaomin <3033358758@qq.com>
2019-04-12 21:47:29 +0800

    "newtree"

---
+++ springcloud-journalism-service/src/main/java/com/jk/SpringcloudJournalismServiceApplication.java
@@ -0,0 +1,16 @@
+package com.jk;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+@MapperScan("com.jk.mapper")
+public class SpringcloudJournalismServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudJournalismServiceApplication.class, args);
+    }
+
+}
